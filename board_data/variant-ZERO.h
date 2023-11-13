f98c0fe3eda62b7aaa3f3c81a5ae1a774718d3e2
shurik179 <shurik179@gmail.com>
2023-11-13 10:50:19 -0500

    template variant.h, variant.cpp

--- board_data/variant-ZERO.h
+++ board_data/variant-ZERO.h
@@ -1,7 +1,4 @@
 /*
-
-
-
   Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
 
   This library is free software; you can redistribute it and/or
@@ -19,8 +16,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _VARIANT_YOZH_
-#define _VARIANT_YOZH_
+#ifndef _VARIANT_ARDUINO_ZERO_
+#define _VARIANT_ARDUINO_ZERO_
 
 // The definitions here needs a SAMD core >=1.6.10
 #define ARDUINO_SAMD_VARIANT_COMPLIANCE 10610
@@ -56,11 +53,14 @@ extern "C"
  *----------------------------------------------------------------------------*/
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (25u)
-#define NUM_DIGITAL_PINS     (17u)
-#define NUM_ANALOG_INPUTS    (7u)
-#define NUM_ANALOG_OUTPUTS   (0u)
-#define analogInputToDigitalPin(p)  ((p < 7u) ? (p) : -1)
+#ifdef __cplusplus
+extern "C" unsigned int PINCOUNT_fn();
+#endif
+#define PINS_COUNT           (PINCOUNT_fn())
+#define NUM_DIGITAL_PINS     (20u)
+#define NUM_ANALOG_INPUTS    (6u)
+#define NUM_ANALOG_OUTPUTS   (1u)
+#define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
 
 #define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
 #define digitalPinToBitMask(P)     ( 1 << g_APinDescription[P].ulPin )
@@ -79,20 +79,25 @@ extern "C"
  */
 // #define digitalPinToTimer(P)
 
+// LEDs
+#define PIN_LED_13           (13u)
+#define PIN_LED_RXL          (25u)
+#define PIN_LED_TXL          (26u)
+#define PIN_LED              PIN_LED_13
+#define PIN_LED2             PIN_LED_RXL
+#define PIN_LED3             PIN_LED_TXL
+#define LED_BUILTIN          PIN_LED_13
 
 /*
- * Pin definitions
+ * Analog pins
  */
-
-
-#define PIN_A0         (0ul)
-#define PIN_A1         (1ul)
-#define PIN_A2         (2ul)
-#define PIN_A3         (3ul)
-#define PIN_A4         (4ul)
-#define PIN_A5         (5ul)
-#define PIN_A6         (6ul)
-
+#define PIN_A0               (14ul)
+#define PIN_A1               (15ul)
+#define PIN_A2               (16ul)
+#define PIN_A3               (17ul)
+#define PIN_A4               (18ul)
+#define PIN_A5               (19ul)
+#define PIN_DAC0             (14ul)
 
 static const uint8_t A0  = PIN_A0;
 static const uint8_t A1  = PIN_A1;
@@ -100,71 +105,75 @@ static const uint8_t A2  = PIN_A2;
 static const uint8_t A3  = PIN_A3;
 static const uint8_t A4  = PIN_A4;
 static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6 ;
+static const uint8_t DAC0 = PIN_DAC0;
+#define ADC_RESOLUTION		12
 
-
-#define PIN_ANALOG_CTRL  (7ul)
-#define PIN_HEARTBEAT    (8ul)
-
-#define PIN_SERVO1       (10ul)
-#define PIN_SERVO2       (9ul)
-
-
-#define PIN_ENC1_DIR     (15ul)
-#define PIN_ENC1_SPEED   (16ul)
-#define PIN_MOTOR1A      (17ul)
-#define PIN_MOTOR1B      (18ul)
-
-#define PIN_ENC2_DIR     (19ul)
-#define PIN_ENC2_SPEED   (20ul)
-#define PIN_MOTOR2A      (22ul)
-#define PIN_MOTOR2B      (21ul)
-
-
-
-
-// dummy
-#define PIN_DAC0         (25ul)
-
+// Other pins
+#define PIN_ATN              (38ul)
+static const uint8_t ATN = PIN_ATN;
 
 /*
  * Serial interfaces
  */
+// Serial (EDBG)
+#define PIN_SERIAL_RX       (31ul)
+#define PIN_SERIAL_TX       (30ul)
+#define PAD_SERIAL_TX       (UART_TX_PAD_2)
+#define PAD_SERIAL_RX       (SERCOM_RX_PAD_3)
 
+// Serial1
+#define PIN_SERIAL1_RX       (0ul)
+#define PIN_SERIAL1_TX       (1ul)
+#define PAD_SERIAL1_TX       (UART_TX_PAD_2)
+#define PAD_SERIAL1_RX       (SERCOM_RX_PAD_3)
 
 /*
  * SPI Interfaces
  */
-#define SPI_INTERFACES_COUNT 0
+#define SPI_INTERFACES_COUNT 1
+
+#define PIN_SPI_MISO         (22u)
+#define PIN_SPI_MOSI         (23u)
+#define PIN_SPI_SCK          (24u)
+#define PERIPH_SPI           sercom4
+#define PAD_SPI_TX           SPI_PAD_2_SCK_3
+#define PAD_SPI_RX           SERCOM_RX_PAD_0
+
+static const uint8_t SS	  = PIN_A2 ;	// SERCOM4 last PAD is present on A2 but HW SS isn't used. Set here only for reference.
+static const uint8_t MOSI = PIN_SPI_MOSI ;
+static const uint8_t MISO = PIN_SPI_MISO ;
+static const uint8_t SCK  = PIN_SPI_SCK ;
 
 /*
  * Wire Interfaces
  */
-#define WIRE_INTERFACES_COUNT 2
-//wire: connection to ItsyBitsy; Yozh is a slave on this bus
-#define PIN_WIRE_SDA             (11ul)
-#define PIN_WIRE_SCL             (12ul)
-#define PERIPH_WIRE              sercom2
-#define WIRE_IT_HANDLER          SERCOM2_Handler
-//wire1: connection to IMU; Yozh is a master on this bus
-#define PIN_WIRE1_SDA            (13ul)
-#define PIN_WIRE1_SCL            (14ul)
-#define PERIPH_WIRE1             sercom1
-#define WIRE1_IT_HANDLER         SERCOM1_Handler
+#define WIRE_INTERFACES_COUNT 1
 
+#define PIN_WIRE_SDA         (20u)
+#define PIN_WIRE_SCL         (21u)
+#define PERIPH_WIRE          sercom3
+#define WIRE_IT_HANDLER      SERCOM3_Handler
+
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
 
 /*
  * USB
  */
-#define PIN_USB_DM          (23ul)
-#define PIN_USB_DP          (24ul)
-//fake - in fact, host enable pin is not connected
-#define PIN_USB_HOST_ENABLE (25ul)
+#define PIN_USB_HOST_ENABLE (27ul)
+#define PIN_USB_DM          (28ul)
+#define PIN_USB_DP          (29ul)
+
 /*
  * I2S Interfaces
  */
-#define I2S_INTERFACES_COUNT 0
+#define I2S_INTERFACES_COUNT 1
 
+#define I2S_DEVICE          0
+#define I2S_CLOCK_GENERATOR 3
+#define PIN_I2S_SD          (9u)
+#define PIN_I2S_SCK         (1u)
+#define PIN_I2S_FS          (0u)
 
 #ifdef __cplusplus
 }
@@ -187,7 +196,17 @@ extern SERCOM sercom3;
 extern SERCOM sercom4;
 extern SERCOM sercom5;
 
+extern Uart Serial;
+extern Uart Serial1;
 
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+unsigned int PINCOUNT_fn();
+#ifdef __cplusplus
+}
 #endif
 
 // These serial port names are intended to allow libraries and architecture-neutral
@@ -205,10 +224,11 @@ extern SERCOM sercom5;
 //
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_USBVIRTUAL      Serial
+#define SERIAL_PORT_USBVIRTUAL      SerialUSB
 #define SERIAL_PORT_MONITOR         Serial
 // Serial has no physical pins broken out, so it's not listed as HARDWARE port
-//#define SERIAL_PORT_HARDWARE        Serial1
-//#define SERIAL_PORT_HARDWARE_OPEN   Serial1
+#define SERIAL_PORT_HARDWARE        Serial1
+#define SERIAL_PORT_HARDWARE_OPEN   Serial1
 
-#endif /* _VARIANT_YOZH_ */
+#endif /* _VARIANT_ARDUINO_ZERO_ */
+
